4895bf3265da624df513ecdf7aafa9f29ae6425b
Alirh1 <alirhayem.1212@gmail.com>
2024-09-18 16:08:17 +0300

    modified index.js <> imported liveCompilerSockets in index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const http = require("http");
+const socketIO = require("socket.io");
+
+const server = http.createServer();
+
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const initLiveCompilerSockets = require("./liveCompilerSockets");
+initLiveCompilerSockets(io);
+
+
+io.on("connection", (socket) => {
+  socket.on("join", ({ classId, userData }) => {
+    socket.join(classId);
+  });
+
+  socket.on("chat-message", (message) => {
+    io.to(message.classId).emit("chat-message", message);
+  });
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
